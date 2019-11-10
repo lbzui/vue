a621fa46a659ce3217406f17896e468eadd93030
bizhong <bizhonglan@gmail.com>
2019-11-10 11:59:49 +0800

    chore: add config files

---
+++ postcss.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  plugins: {
+    'postcss-preset-env': {
+      stage: 3,
+      autoprefixer: {
+        flexbox: 'no-2009',
+        grid: 'no-autoplace'
+      },
+      importFrom: [
+        'src/assets/css/variables/variables.css'
+      ]
+    },
+    'postcss-pxtorem': {
+      unitPrecision: 8,
+      propList: [
+        'font',
+        'font-size',
+        'line-height'
+      ]
+    }
+  }
+}
